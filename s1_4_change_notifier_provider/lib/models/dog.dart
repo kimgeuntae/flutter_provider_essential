345640489ef207f8b41e47c6bcdd24a1281cc181
kimgeuntae <kok617@naver.com>
2023-09-12 18:27:02 +0900

    Add: default set

---
+++ s1_4_change_notifier_provider/lib/models/dog.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Dog with ChangeNotifier {
+  final String name;
+  final String breed;
+  int age;
+
+  Dog({
+    required this.name,
+    required this.breed,
+    this.age = 1,
+  });
+
+  void grow() {
+    age++;
+    // 상태 변화 감지
+    notifyListeners();
+    print('age: $age');
+  }
+}
