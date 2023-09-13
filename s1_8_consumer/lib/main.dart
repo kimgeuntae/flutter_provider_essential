ba301133767ab0c676b3caee0c187c09919db722
kimgeuntae <kok617@naver.com>
2023-09-13 14:12:05 +0900

    Ref: Consumer로 변경

--- s1_8_consumer/lib/main.dart
+++ s1_8_consumer/lib/main.dart
@@ -12,9 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => Dog(name: 'dog05', breed: 'breed05', age: 3),
+      create: (context) => Dog(name: 'dog08', breed: 'breed08', age: 3),
       child: MaterialApp(
-        title: 'Provider 05',
+        title: 'Provider 08',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
@@ -37,23 +37,25 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Provider 05'),
+        title: Text('Provider 08'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              // '- name: ${Provider.of<Dog>(context, listen: false).name}',
-              '- name: ${context.watch<Dog>().name}', // select 써도 괜찮
-              // watch: listen object의 변화를 감지하여 리빌드
-              style: TextStyle(fontSize: 20.0),
+      body: Consumer<Dog>(
+        builder: (BuildContext context, Dog dog, Widget? child) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  '- name: ${dog.name}',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                SizedBox(height: 10.0),
+                BreedAndAge(),
+              ],
             ),
-            SizedBox(height: 10.0),
-            BreedAndAge(),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
@@ -64,17 +66,19 @@ class BreedAndAge extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          // '- breed: ${Provider.of<Dog>(context, listen: false).breed}',
-          '- breed: ${context.select<Dog, String>((Dog dog) => dog.breed)}',
-          // select: listen 하고 싶은 프로퍼티(breed)의 변화만 감지하여 리빌드
-          style: TextStyle(fontSize: 20.0),
-        ),
-        SizedBox(height: 10.0),
-        Age(),
-      ],
+    return Consumer<Dog>(
+      builder: (BuildContext _, Dog dog, Widget? __) {
+        return Column(
+          children: [
+            Text(
+              '- breed: ${dog.breed}',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 10.0),
+            Age(),
+          ],
+        );
+      },
     );
   }
 }
@@ -84,24 +88,25 @@ class Age extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          // '- age: ${Provider.of<Dog>(context).age}',
-          '- age: ${context.select<Dog, int>((Dog dog) => dog.age)}',
-          // select: listen 하고 싶은 프로퍼티(age)의 변화만 감지하여 리빌드
-          style: TextStyle(fontSize: 20.0),
-        ),
-        SizedBox(height: 20.0),
-        ElevatedButton(
-          onPressed: () => context.read<Dog>().grow(),
-          // 버튼은 리빌드 될 필요가 없기때문에 listen false를 강제하기 때문에 read 를 써서 listen false 해줘야함
-          child: Text(
-            'Grow',
-            style: TextStyle(fontSize: 20.0),
-          ),
-        ),
-      ],
+    return Consumer<Dog>(
+      builder: (_, Dog dog, __) {
+        return Column(
+          children: [
+            Text(
+              '- age: ${dog.age}',
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () => dog.grow(),
+              child: Text(
+                'Grow',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
