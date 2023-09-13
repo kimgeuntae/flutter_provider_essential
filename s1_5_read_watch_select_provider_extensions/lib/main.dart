9d06e0b9227bf6c6e5571c6fa75998b8ca918669
kimgeuntae <kok617@naver.com>
2023-09-13 11:25:58 +0900

    Add: read, watch, select test

--- s1_5_read_watch_select_provider_extensions/lib/main.dart
+++ s1_5_read_watch_select_provider_extensions/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => Dog(name: 'dog05', breed: 'breed05'),
+      create: (context) => Dog(name: 'dog05', breed: 'breed05', age: 3),
       child: MaterialApp(
         title: 'Provider 05',
         debugShowCheckedModeBanner: false,
@@ -45,7 +45,9 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              '- name: ${Provider.of<Dog>(context, listen: false).name}',
+              // '- name: ${Provider.of<Dog>(context, listen: false).name}',
+              '- name: ${context.watch<Dog>().name}', // select 써도 괜찮
+              // watch: listen object의 변화를 감지하여 리빌드
               style: TextStyle(fontSize: 20.0),
             ),
             SizedBox(height: 10.0),
@@ -65,7 +67,9 @@ class BreedAndAge extends StatelessWidget {
     return Column(
       children: [
         Text(
-          '- breed: ${Provider.of<Dog>(context, listen: false).breed}',
+          // '- breed: ${Provider.of<Dog>(context, listen: false).breed}',
+          '- breed: ${context.select<Dog, String>((Dog dog) => dog.breed)}',
+          // select: listen 하고 싶은 프로퍼티(breed)의 변화만 감지하여 리빌드
           style: TextStyle(fontSize: 20.0),
         ),
         SizedBox(height: 10.0),
@@ -83,14 +87,15 @@ class Age extends StatelessWidget {
     return Column(
       children: [
         Text(
-          '- age: ${Provider.of<Dog>(context).age}',
-          // 리 랜더링이 필요한 부분은 listen false를 주면 안됨
+          // '- age: ${Provider.of<Dog>(context).age}',
+          '- age: ${context.select<Dog, int>((Dog dog) => dog.age)}',
+          // select: listen 하고 싶은 프로퍼티(age)의 변화만 감지하여 리빌드
           style: TextStyle(fontSize: 20.0),
         ),
         SizedBox(height: 20.0),
         ElevatedButton(
-          onPressed: () => Provider.of<Dog>(context, listen: false).grow(),
-          // UI를 변경시킬 필요가 없으므로 listen false
+          onPressed: () => context.read<Dog>().grow(),
+          // 버튼은 리빌드 될 필요가 없기때문에 listen false를 강제하기 때문에 read 를 써서 listen false 해줘야함
           child: Text(
             'Grow',
             style: TextStyle(fontSize: 20.0),
