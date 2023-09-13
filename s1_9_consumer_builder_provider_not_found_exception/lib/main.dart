6ea77f9682948aa48ab6ba9107585974904b76d4
kimgeuntae <kok617@naver.com>
2023-09-13 17:25:04 +0900

    Fix: 빌더 패턴을 사용하여 ProviderNotFoundException 를 해결

--- s1_9_consumer_builder_provider_not_found_exception/lib/main.dart
+++ s1_9_consumer_builder_provider_not_found_exception/lib/main.dart
@@ -41,6 +41,33 @@ class MyHomePage extends StatelessWidget {
       ),
       body: ChangeNotifierProvider(
         create: (_) => Foo(),
+        builder: (BuildContext context, Widget? _) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  // 중간에 빌더를 넣어줌으로써 return 위젯이 하위로 가고, 빌더의 context를 사용하면서 provider 를 사용할 수 있게됨.
+                  '${context.watch<Foo>().value}',
+                  style: TextStyle(fontSize: 40),
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () => context.read<Foo>().changeValue(),
+                  child: Text(
+                    'Change Value',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      /*
+      // 조상 context를 가져다 써서 ProviderNotFoundException 가 발생함.
+      body: ChangeNotifierProvider(
+        create: (_) => Foo(),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -61,6 +88,7 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
       ),
+    */
     );
   }
 }
